1dabea1c3f3bb69734a0bbce93af15ad26ef75e9
tommooredotbe <mooretom@outlook.com>
2015-11-16 20:13:57 +0100

    contactform v3341

--- mail/contact_us.php
+++ mail/contact_us.php
@@ -1,8 +1,4 @@
 <?php
-//error_reporting(E_ALL);
-//ini_set("display_errors", 1);
-
-// Check for empty fields
 if(empty($_POST['subject'])  		||
    empty($_POST['emailadres']) 		||
    empty($_POST['bericht'])	||
@@ -16,17 +12,12 @@ $subject = $_POST['subject'];
 $email_address = $_POST['emailadres'];
 $message = $_POST['bericht'];
 
-//print_r($_POST);
-	
-// Create the email and send the message
 $to = 'info@designosource.be'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $subject";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nEmail: $email_address\n\Subject: $subject\n\nMessage:\n$message";
 $headers = "From: noreply@designosource.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 mail($to,$email_subject,$email_body,$headers);
-//return true;		
-
-//print_r($headers);
+return true;		
 ?>
 
 
